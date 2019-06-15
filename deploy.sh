eae1c3aba902d5e35d03953e6b76cef399d9c5b2
Thomas Groshong <thomas.groshong@gmail.com>
2019-06-15 17:30:08 -0400

    Fix deploy.sh script

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,9 @@
 set -e
 
 echo "Building site with hugo..."
-hugo.exe
+hugo
 
 echo "Copying files to S3..."
-aws.cmd s3 cp --recursive public s3://criticalgamemastery.com --cache-control max-age=500 --acl public-read
+aws s3 cp --recursive public s3://criticalgamemastery.com --cache-control max-age=500 --acl public-read
 
 echo "Success!"
